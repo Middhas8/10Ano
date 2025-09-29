eb03443594dccd05912f9bd80582400ea09105fa
Middhas <joaquim.tavares.costa.8@gmail.com>
2025-09-29 10:45:04 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<<"Fernando, a sua mae e tao velha mas tao velha que quando ela da leite, sai leite em po";
+    std::cout<< "Eu posso falar isto pois ele ja riu do meu tio morto";
+    return 0;
+}
