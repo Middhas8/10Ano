850f68ef13aff90486d5268e2b9ad8b266892170
Middhas <joaquim.tavares.costa.8@gmail.com>
2025-10-02 15:05:37 +0100

    Add files via upload

---
+++ Progamação/C++/ParOuImpar.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+	//Codigo começa a partir desta linha
+	float num
+		cout << "Me de um numero";
+	cin >> num;
+	if (num < 0) {
+		cout << "Este numero e negativo";
+		}
+	else {
+		cout << "Este numero e negativo";
+	}
+
+	return 0;
+}
