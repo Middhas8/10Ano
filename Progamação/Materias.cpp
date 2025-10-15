9c71a30a8580e564412d26ef17f0687a3e8dcfc8
Middhas <joaquim.tavares.costa.8@gmail.com>
2025-10-15 10:29:58 +0100

    Add files via upload

---
+++ Progamação/Materias.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	do {
+		int num;
+		char sair = 'N';
+		string materia[5] = { "Portugues", "Ingles", "Historia", "Matematica", "Geografia" };
+		cout << "Qual numero de materia voce quer? \n";
+		cin >> num;
+		cout << "A materia equivalente a " << num << " e " << materia[num];
+		return 0;
+	} while ('sair' == 'N');
+}
