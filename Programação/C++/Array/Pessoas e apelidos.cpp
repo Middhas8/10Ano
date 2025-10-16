dd6f0adfb2f2242c2755b88e3c2ff9709ad3b07e
Middhas <joaquim.tavares.costa.8@gmail.com>
2025-10-16 22:00:40 +0100

    Add files via upload

---
+++ Programação/C++/Array/Pessoas e apelidos.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <locale.h>
+#include <string>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+	int N;
+	char sair = 'n';
+	string nomes[5];
+	string apelidos[5];
+
+
+
+	for (int i = 0; i < 5; i++) {
+		cout << "Me de o nome da " << i + 1 << "º pessoa \n";
+		cin >> nomes[i];
+	}
+	for (int i = 0; i < 5; i++) {
+		cout << "Me de o apelido de " << nomes[i] << "\n";
+		cin >> apelidos[i];
+	}
+
+	while (sair == 'n') {
+		cout << "Diz um número para saber a pessoa e a seu apelido carinhoso: ";
+		cin >> N;
+		cout << "A pessoa é " << nomes[N - 1] << ", tendo o apelido " << apelidos[N - 1];
+		cout << "\n\nDeseja sair (s/n)";
+		cin >> sair;
+	}
+}
