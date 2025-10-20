91655e2b054da98f6368aaeccb66dc5c82669b9a
Middhas <joaquim.tavares.costa.8@gmail.com>
2025-10-20 10:59:04 +0100

    Refactor temperature converter logic and remove unused code

--- Programação/C++/ConverterTemperatura.cpp
+++ Programação/C++/ConverterTemperatura.cpp
@@ -1,31 +1,25 @@
 #include <iostream>
 #include <locale.h>
 #include <string>
-
 using namespace std;
-int main() {
-
-	setlocale(LC_ALL, "Portuguese");
-	float qn = 0, sn = 0, n;//qn= quantos negativos, sn=soma negativos, n=numero
-	char es = 'N';
-	for (int i = 0; i < 10; i++)	{
-		cout << "Digite um numero : ";
-		cin >> n;
-		if (n<0) {
-			qn = qn++;
-			sn = sn + n;
-		}
+int main()
+ 
+{
+	setlocale(LC_ALL, "portuguese");
+	char EscTemp;//Escolha de temperatura, C = celsius, F = fahrenheit.
+	float Temp;
+	cout << "Como voce quer converter a temperatura, De fahrenheit para celsius(C), ou celsius a fahrenheit(F)?\n";
+	cin >> EscTemp;
+	cout << "Me de uma temperatura : ";
+	cin >> Temp;
+	if (EscTemp == 'C') {
+		cout << "A temperatura convertida para celcius e igual a " << (Temp * 1.8) - 32;
 	}
-		cout << "Deseja saber quantos negativos voce escreveu(N) ou quer saber a soma deles(S)? (N/S) : ";
-		cin >> es;
-		if (es == 'N') {
-			cout << "Voce escreveu " << qn << " numeros";
-		}
-		else if (es == 'S') {
-			cout << "A soma dos numeros negativos e igual a " << sn;
-		}
-		else {
-			cout << "Eu coloquei duas opções e você de alguma forma errou";
-		}
-	
+	else if (EscTemp == 'F') {
+		cout << "A temperatura convertida para fahrenheit e igual a " << (Temp * 1.8 ) + 32 ;
+	}
+	else {
+		cout << "Tinha apenas duas opÃ§Ãµes e voce errou de algum jeito";
+	}
 }
+ 
