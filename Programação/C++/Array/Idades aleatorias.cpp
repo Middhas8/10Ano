8ef9b2462c4e83d7c9b194b0a7bf66b86f6d552f
Middhas <joaquim.tavares.costa.8@gmail.com>
2025-10-22 11:23:01 +0100

    Update Idades aleatorias.cpp

--- Programação/C++/Array/Idades aleatorias.cpp
+++ Programação/C++/Array/Idades aleatorias.cpp
@@ -5,8 +5,6 @@
 #include <time.h>
 using namespace std;
 
-
-
 int main()
 {
 	setlocale(LC_ALL, "Portuguese");
@@ -14,16 +12,12 @@ int main()
 	int r;
 	string nomes[5] = { "A","B","C","D","E" };
 	int idades[5] = {};
-	for (int i = 0; i < 5; i++) {
-		idades[i] = (rand() % 40) + 15;
+	for (int i = 0; i < 5; i++)
+	{
+		idades[i] = (rand() % 40) + 15; 
 	} 
 	cout << "Quem queres saber a idade?(1-5)";
 	cin >> r;
-	cout << "A idade de " << nomes[r - 1] << " é " << idades[r - 1] << " anos." << endl;
-	
-
-
+	cout << "A idade de " << nomes[r - 1] << " Ã© " << idades[r - 1] << " anos.\n";
 	return 0;
 }
-
-
