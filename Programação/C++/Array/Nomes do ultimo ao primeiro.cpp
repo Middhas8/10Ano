55753614efb0b9b77230142ec0d536caadf318b9
Middhas <joaquim.tavares.costa.8@gmail.com>
2025-10-23 14:55:17 +0100

    Update Nomes do ultimo ao primeiro.cpp

--- Programação/C++/Array/Nomes do ultimo ao primeiro.cpp
+++ Programação/C++/Array/Nomes do ultimo ao primeiro.cpp
@@ -10,12 +10,17 @@ int main()
 	setlocale(LC_ALL, "Portuguese");
 	srand(time(0)); //Ignore, nao e utilizado neste codigo
 	string No[5]; //No=Nome
+	char Es = 'N'; //Es=Escolha
+    do {
     for (int i = 0; i < 5; i++) {
 	cout << "Me de o " << i+1 << "º nome : ";
 	cin >> No[i];
-	}
+	    }
 	for (int i = 4; i >= 0; i--) {
 	cout << No[i] << endl;
-	}
-	return 0; 
+	    }
+	cout << "Deseja sair (S/N)";
+	cin >> Es;
+    } while (Es != 'S');
+	return 0;
 }
