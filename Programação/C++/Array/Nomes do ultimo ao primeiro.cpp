d3b67505a67b2cfe5383177005a9dd8856e3b5c1
Middhas <joaquim.tavares.costa.8@gmail.com>
2025-10-23 14:19:13 +0100

    Create Nomes do ultimo ao primeiro.cpp

---
+++ Programação/C++/Array/Nomes do ultimo ao primeiro.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <locale.h>
+#include <cstdlib>
+#include <time.h>
+using namespace std;
+ 
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+	srand(time(0)); //Ignore, nao e utilizado neste codigo
+	string No[5]; //No=Nome
+    for (int i = 0; i < 5; i++) {
+	cout << "Me de o " << i+1 << "º nome : ";
+	cin >> No[i];
+	}
+	for (int i = 4; i >= 0; i--) {
+	cout << No[i] << endl;
+	}
+	return 0; 
+}
