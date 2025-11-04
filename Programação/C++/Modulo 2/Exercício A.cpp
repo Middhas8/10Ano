6e29ed7bdb68faeddf190a19f06bf641abd3fba1
Middhas <joaquim.tavares.costa.8@gmail.com>
2025-11-04 10:53:45 +0000

    04/11/2025
    Problemas Introdutório da Mooshak sobre: Festa do Ricardo

---
+++ Programação/C++/Modulo 2/Exercício A.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <locale.h>
+#include <cstdlib>
+#include <time.h>
+using namespace std;
+int main() { 
+	int N, M;
+	cin >> N >> M;
+	cout << N / M;
+}
