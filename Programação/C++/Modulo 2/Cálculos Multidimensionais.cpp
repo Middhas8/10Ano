5fc61b401c985080338452ffd25f73eabf578758
Middhas <joaquim.tavares.costa.8@gmail.com>
2025-11-25 21:21:43 +0000

    Refactor matrix calculation program structure

--- Programação/C++/Modulo 2/Cálculos Multidimensionais.cpp
+++ Programação/C++/Modulo 2/Cálculos Multidimensionais.cpp
@@ -7,36 +7,80 @@ using namespace std;
 void bar() {
 	cout << "\n============================\n";
 }
+void bar2() {
+	cout << "\n----------------------------\n";
+}
 int main()
 {
 	setlocale(LC_ALL, "Portuguese");
 	srand(time(0));
-	int num=0, b, i, j, Matriz1[5][3], Matriz2[5][3], Soma[5][3] = {0}; //primeiro [i] == disciplina, segundo [i] == nota
+	int escolha = 1, num = 1, b, i, j, Matriz1[5][3], Matriz2[5][3], Cálculos[5][3];
 	for (b = 0; b < 2; b++) {//b == Qual matriz
-			bar();
-			cout << b + 1 << "º Matriz	";
-			bar();
+		bar();
+		cout << b + 1 << "º Matriz	";
+		bar();
 		for (i = 0; i < 5; i++) {// i == Linha da matriz
 			for (j = 0; j < 3; j++) {// j == Qual numero
-				if (j==0) {
-					cout << "\nLinha número " << i+1;
+				if (j == 0) {
+					cout << "\nLinha número " << i + 1;
 					bar();
 				}
-			cout << j+1 <<"º número : ";
-			if (b==0) {
-				cin >> Matriz1[i][j];
-			} else {
-				cin >> Matriz2[i][j];
-			}
-		
-			if (j==2)
-			{
-				system("CLS");
-			}
+				cout << j + 1 << "º número : ";
+				if (b == 0) {
+					cin >> Matriz1[i][j];
+				}
+				else {
+					cin >> Matriz2[i][j];
+				}
+
+				if (j == 2)
+				{
+					system("CLS");
+				}
 			}
 		}
-    }
-	bar();
-	cout << "Qual calculo queres fazer?\n"; bar();
-	cout << "          Adição\n\n          Multiplicação\n\n          Subtração\n"; bar();
+	}
+	do {
+		bar();
+		cout << "Qual calculo queres fazer?";
+		bar2();
+		cout << "        Adição[1]";	bar2();
+		cout << "       Subtração[2]";	bar2();
+		cout << "     Multiplicação[3]";	bar2();
+		cout << "    Nada, quero sair[0]";	bar();
+
+		cin >> escolha;
+		system("CLS");
+		for (i = 0; i < 5; i++) {
+			for (j = 0; j < 3; j++) {
+				if (escolha == 1) {
+					Cálculos[i][j] = Matriz2[i][j] + Matriz1[i][j];
+				}
+				else if (escolha == 2) {
+					Cálculos[i][j] = Matriz2[i][j] - Matriz1[i][j];
+				}
+				else if (escolha == 3) {
+					Cálculos[i][j] = Matriz2[i][j] * Matriz1[i][j];
+				} else {
+					break;
+				} 
+				if (j == 0) {
+					cout << " | " << Cálculos[i][j];
+				} else if (j == 1) {
+					cout << " : " << Cálculos[i][j];
+				} else {
+					cout << " : " << Cálculos[i][j] << " | \n";
+				}
+
+				if (j == 2) {
+					cout << endl;
+				}
+				if (i == 4 && j == 2) {
+					cout << "\nQueres fazer outro calculo? [ 0(Sair) / 1(Outro calculo) ] : ";
+					cin >> escolha;
+					break;
+				}
+			}
+		}
+	} while (escolha != 0);
 }
