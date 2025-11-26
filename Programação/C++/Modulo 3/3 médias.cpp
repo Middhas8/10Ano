276faab2fad9293a38aac0211b6c091381b3da9c
Middhas <joaquim.tavares.costa.8@gmail.com>
2025-11-26 11:36:20 +0000

    Refactor to handle up to 10 students and 10 grades

--- Programação/C++/Modulo 3/3 médias.cpp
+++ Programação/C++/Modulo 3/3 médias.cpp
@@ -13,34 +13,35 @@ void bar() {
 int main()
 {
 	setlocale(LC_ALL, "Portuguese");
-	int  i, j, turma_notas[3][3];
-	float med[3]{ 0,0,0 };
-	for (i = 0; i < 3; i++) {
+	int  i, j, turma_notas[10][10], med[10]{ 0,0,0,0,0,0,0,0,0,0 };
+	for (i = 0; i < 10; i++) {
+		system("CLS");
 		cout << "Notas " << i + 1 << "º Aluno \n";
 		bar();
-		for (j = 0; j < 3; j++) {
+		for (j = 0; j < 10; j++) {
 			cout << j + 1 << "º Nota : ";
 			cin >> turma_notas[i][j];
-			if (turma_notas[i][j]<0 || turma_notas[i][j]>20) {
-				cout <<"Nota invalida, numero apenas entre 0 a 20\n";
+			if (turma_notas[i][j] < 0 || turma_notas[i][j]>20) {
+				cout << "Nota invalida, numero apenas entre 0 a 20\n";
 				j--;
-			} else {
+			}
+			else {
 				med[i] = med[i] + turma_notas[i][j];
-				if (j == 2) {
-					med[i] = med[i] / 3;
-					if (i != 2)
+				if (j == 9) {
+					med[i] = med[i] / 10;
+					if (i != 9)
 					{
 						bar();
 					}
 				}
-			}	
+			}
 		}
 	}
 
-	for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < 10; i++) {
 		bar();
-		cout << "\nMédia das notas do " << i + 1 << "º aluno : " << med[i] <<  "\n\n";
-		if (i==2) {
+		cout << "\nMédia das notas do " << i + 1 << "º aluno : " << med[i] << "\n\n";
+		if (i == 9) {
 			bar();
 		}
 	}
