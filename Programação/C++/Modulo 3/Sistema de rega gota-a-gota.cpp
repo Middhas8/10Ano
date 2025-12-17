a8642067e17c70c2256af6b890bb5470c1500dc2
Middhas <joaquim.tavares.costa.8@gmail.com>
2025-12-17 10:20:04 +0000

    Implement irrigation data analysis program in C++
    Added a C++ program to analyze irrigation data based on user input for minimum and maximum values, intervals, and days.

--- Programação/C++/Modulo 3/Sistema de rega gota-a-gota.cpp
+++ Programação/C++/Modulo 3/Sistema de rega gota-a-gota.cpp
@@ -1,1 +1,32 @@
-//colocar qnd chegar em casa
+#include <iostream>
+#include <string>
+#include <locale.h>
+#include <cstdlib>
+#include <time.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+	int VMin, VMax, Int[2], i, j, Dias, aux = 0, DMin = 0, DMax = 0, DAde = 0;//Val[0]=Valor Minimo/Val[1]=Máximo; Int[0]=Intervalo Minimo/Int[1] Máximo
+	string Gotas;
+	cin >> VMin >> VMax >> Int[0] >> Int[1] >> Dias;
+	for ( i = 0; i < Dias; i++) {
+		aux = 0;
+		cin >> Gotas;
+		for ( j = 0; j < Gotas.size(); j++) {
+			if (Gotas[j] == '1') {
+				aux++;
+			}
+		}
+		if (aux < VMin) {
+			DMin++;
+		} else if (aux > VMax) {
+			DMax++;
+		}
+		else if (aux >= Int[0] && aux <= Int[1]) {
+			DAde++;
+		}
+	}
+	cout << DMin << " " << DMax << " " << DAde;
+	return 0;
+}
