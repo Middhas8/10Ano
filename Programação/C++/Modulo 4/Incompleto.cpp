30627d19a261375128f2c2eb024ae76fed0551b1
Middhas <joaquim.tavares.costa.8@gmail.com>
2026-01-07 22:59:57 +0000

    Add menu-driven number processing functions
    Implemented a menu-driven program to calculate the average, minimum, and maximum of user input numbers.

--- Programação/C++/Modulo 4/Incompleto.cpp
+++ Programação/C++/Modulo 4/Incompleto.cpp
@@ -1,1 +1,64 @@
-//Colocar exercicio completo de funções, exercicio 1
+/*
+//AS FUNÇÕES NAO FUNCIONAM, badum-ts
+#include <iostream>
+#include <string>
+#include <locale.h>
+#include <cstdlib>
+#include <time.h>
+using namespace std;
+int Tamanho = 10;
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+	int Opc = 1, Numero[10] = {0}, Resultado;
+	string Bar1 = "\n==============================\n", Bar2 = "\n------------------------------\n";
+	for (int i = 0; i < Tamanho; i++) {
+		cout << i + 1 << "º Numero : ";
+		cin >> Numero[i];
+	}
+	do {
+		system("cls");
+		cout << Bar1 << "            MENU"<<Bar1<<"     1 : média dos números"<<Bar2<<"     2 : achar o menor"<<Bar2<<"     3 : achar o maior"<<Bar2<< "     0 : sair" << Bar1;
+		cin >> Opc;
+		switch (Opc)
+		{
+		case 1:
+ 
+			cout << Med(Numero, Resultado);
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 0:
+			cout << "Tchau";
+			break;
+		default:
+			cout << "Opção Incorreta, tente novamente.";
+			break;
+		}
+		if (Opc != 0) {
+			cout << "Pressione enter para continuar...";
+			cin.get();
+			cin.ignore();
+		}
+ 
+	} while (Opc!=0);
+ 
+return 0;
+}
+ 
+int Med(int n[], int res) {
+	int res = 0;
+	for (int i = 0; i < Tamanho; i++) {
+		res += n[i];
+	}
+	res = res / Tamanho;
+	return res;
+}
+int Men() {
+ 
+}
+int Mai() {
+ 
+*/
