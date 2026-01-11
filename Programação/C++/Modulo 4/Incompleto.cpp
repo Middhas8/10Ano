3dbddd3ca2f7d52805e63e269ad82931465c5e66
Middhas <joaquim.tavares.costa.8@gmail.com>
2026-01-11 22:10:04 +0000

    Update Incompleto.cpp

--- Programação/C++/Modulo 4/Incompleto.cpp
+++ Programação/C++/Modulo 4/Incompleto.cpp
@@ -1,64 +1,93 @@
-/*
-//AS FUNÇÕES NAO FUNCIONAM, badum-ts
-#include <iostream>
-#include <string>
-#include <locale.h>
-#include <cstdlib>
+#include <iostream> 
+#include <string> 
+#include <locale.h> 
+#include <cstdlib> 
 #include <time.h>
 using namespace std;
+
 int Tamanho = 10;
-int main()
-{
-	setlocale(LC_ALL, "Portuguese");
-	int Opc = 1, Numero[10] = {0}, Resultado;
-	string Bar1 = "\n==============================\n", Bar2 = "\n------------------------------\n";
-	for (int i = 0; i < Tamanho; i++) {
-		cout << i + 1 << "º Numero : ";
-		cin >> Numero[i];
+
+float Med(int n[]) {
+int res = 0;
+float media;
+
+for (int i = 0; i < Tamanho; i++) {
+    res += n[i];
 	}
-	do {
-		system("cls");
-		cout << Bar1 << "            MENU"<<Bar1<<"     1 : média dos números"<<Bar2<<"     2 : achar o menor"<<Bar2<<"     3 : achar o maior"<<Bar2<< "     0 : sair" << Bar1;
-		cin >> Opc;
-		switch (Opc)
-		{
-		case 1:
- 
-			cout << Med(Numero, Resultado);
-			break;
-		case 2:
-			break;
-		case 3:
-			break;
-		case 0:
-			cout << "Tchau";
-			break;
-		default:
-			cout << "Opção Incorreta, tente novamente.";
-			break;
-		}
-		if (Opc != 0) {
-			cout << "Pressione enter para continuar...";
-			cin.get();
-			cin.ignore();
-		}
- 
-	} while (Opc!=0);
- 
+media = res;
+media = media / Tamanho;
+return media;
+}
+
+int Men(int n[]) {
+int menor = n[0];
+for (int i = 1; i < Tamanho; i++) {
+    if (n[i] < menor) {
+        menor = n[i];
+    }
+	}
+return menor;
+}
+
+int Mai(int n[]) {
+int maior = n[0];
+for (int i = 1; i < Tamanho; i++) {
+    if (n[i] > maior) {
+        maior = n[i];
+    }
+	}
+return maior;
+}
+
+int main() {
+setlocale(LC_ALL, "Portuguese");
+
+int Opc = 1;
+int Numero[10] = { 0 };
+
+for (int i = 0; i < Tamanho; i++) {
+    cout << i + 1 << "º Numero : ";
+    cin >> Numero[i];
+	}
+
+string Bar1 = "\n==============================\n";
+string Bar2 = "\n------------------------------\n";
+
+do {
+    system("cls");
+    cout << Bar1 << "            MENU" << Bar1
+        << "     1 : média dos números" << Bar2
+        << "     2 : achar o menor" << Bar2
+        << "     3 : achar o maior" << Bar2
+        << "     0 : sair" << Bar1;
+
+    cin >> Opc;
+
+    switch (Opc) {
+    case 1:
+        cout << "Média = " << Med(Numero);
+        break;
+    case 2:
+        cout << "Menor = " << Men(Numero);
+        break;
+    case 3:
+        cout << "Maior = " << Mai(Numero);
+        break;
+    case 0:
+        cout << "Tchau";
+        break;
+    default:
+        cout << "Opção Incorreta, tente novamente.";
+        break;
+    }
+
+    if (Opc != 0) {
+        cout << "\nPressione enter para continuar...";
+        cin.ignore();
+        cin.get();
+    }
+
+	} while (Opc != 0);
+
 return 0;
 }
- 
-int Med(int n[], int res) {
-	int res = 0;
-	for (int i = 0; i < Tamanho; i++) {
-		res += n[i];
-	}
-	res = res / Tamanho;
-	return res;
-}
-int Men() {
- 
-}
-int Mai() {
- 
-*/
