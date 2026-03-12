636fbe32ffd5bf84e65270ec630a62f7e28b3fd8
Middhas <joaquim.tavares.costa.8@gmail.com>
2026-03-12 09:58:09 +0000

    Create TreinoA.cpp

---
+++ Programação/C++/Outros/Mooshak/TreinoA.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <locale.h>
+#include <cstdlib>
+#include <time.h>
+#include <cctype>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    string CasosDeTeste[20];
+    int T_NumCasosTestes, NumAlunos, TrocasNecessarias, Alunos[2];
+    cin >> T_NumCasosTestes;
+    for (int i = 0; i < T_NumCasosTestes; i++) {
+    TrocasNecessarias = 0;
+    cin >> NumAlunos; 
+        for (int ii = 0; ii  < NumAlunos; ii ++)
+        {
+            
+            if (ii == 0)
+            {
+                cin >> Alunos[0];
+            }
+            else
+            {
+                cin >> Alunos[1];
+                if (!(Alunos[0] < Alunos[1]))
+                {
+                    TrocasNecessarias++;
+                }
+                else
+                {
+                    Alunos[0] = Alunos[1];
+                }
+            
+            }
+            if (TrocasNecessarias > 1)
+                CasosDeTeste[i] = "NAO\n";
+            else
+                CasosDeTeste[i] = "SIM\n";
+            }
+        }
+
+    for (int i = 0; i < T_NumCasosTestes; i++)
+    {
+        cout << CasosDeTeste[i];
+    }
+    return 0;
+    //Ainda e necessario testar na Mooshark
+}
